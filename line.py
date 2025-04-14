0ba26b0ed8055c7184d2f7acd6226c9130cfb722
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-04-14 14:52:10 -0300

    line.py

--- line.py
+++ line.py
@@ -1,2 +1,33 @@
 def line():
     print("TO DO")
+    import math
+    A = float(input("Ingrese el coeficiente A: "))
+    B = float(input("Ingrese el coeficiente B: "))
+    X1 = float(input("Ingrese el coeficiente X1: "))
+    X2 = float(input("Ingrese el coeficiente X2: "))
+    Y1 = (A * X1 + B)
+    Y2 = (A * X2 + B)
+    
+    p = [X1, Y1]
+    q = [X2, Y2]
+    P1_str = (f"P1 ({X1}, {Y1})")
+    P2_str = (f"P2 ({X2}, {Y2})")
+    
+    # print (A)
+    # print (B)
+    # print (C_X1)
+    # print (C_X2)
+    print(f"El coeficiente A de su ecuación de la recta es: {A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
+    print("")
+    print("Para la siguiente ecuación:")
+    print(f"\tY = {A}X + {B}")
+    print("")
+    print("Dados los siguientes puntos:")
+    print(f"\t{P1_str}")
+    print(f"\t{P2_str}")
+    print("")
+    Distancia = (math.dist(p, q))
+    print(f"La distancia entre ellos es: {Distancia}")
